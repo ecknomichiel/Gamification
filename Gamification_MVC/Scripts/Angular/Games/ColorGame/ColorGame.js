c67596c46a54468e471c2c2455ba038884925c8d
AhAh10 <ahmadi2001@yahoo.com>
2017-07-18 13:06:30 +0200

    Game updated

--- Gamification_MVC/Scripts/Angular/Games/ColorGame/ColorGame.js
+++ Gamification_MVC/Scripts/Angular/Games/ColorGame/ColorGame.js
@@ -1,5 +1,28 @@
 ﻿var app = angular.module("Game4", []);
 
+
 app.controller("ColorGameCr", ["$scope", "$http", function ($scope, $http) {
-    $scope.color = "testing testing!";
+    $scope.color = " Text Color!";
+    var color = ["Blue ", "Green", "Red", "Black", "Yellow "];
+
+    $scope.myFunc = function (color) {
+        if (color == 'blue') alert("correct!");
+        if (color == 'yellow') alert("correct!");
+        if (color == 'red') alert("correct!");
+        if (color == 'green') alert("correct!");
+        if (color == 'black') alert("correct!");
+
+        else alert("Wrong!");
+
+        $scope.count++;
+    };
+
 }]);
+
+
+
+
+
+
+
+
