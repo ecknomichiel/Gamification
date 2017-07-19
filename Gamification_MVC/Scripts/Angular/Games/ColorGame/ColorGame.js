f45a611db295c0adc68281242563bc6b6ee45a6d
AhAh10 <ahmadi2001@yahoo.com>
2017-07-19 14:20:10 +0200

    Added new stuff

--- Gamification_MVC/Scripts/Angular/Games/ColorGame/ColorGame.js
+++ Gamification_MVC/Scripts/Angular/Games/ColorGame/ColorGame.js
@@ -3,14 +3,17 @@
 
 app.controller("ColorGameCr", ["$scope", "$http", function ($scope, $http) {
     $scope.color = " Text Color!";
+    $scope.points = 0;                                                                 //Score
     var colors = ["Blue", "Yellow", "Red", "Green", "Black", "Brown", "Gray", "Magenta"];
     var tal = Math.floor((Math.random() * 8));
     //alert(tal + " : " +color[tal]);
     $scope.color = colors[tal];
 
     $scope.myFunc = function (color) {
-        if (color == $scope.color)
+        if (color == $scope.color) {
             alert("correct!");
+            $scope.points++;                                                    //Score
+        }
         else alert("Wrong!");
  
     };
