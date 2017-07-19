94cdc573f1a63a0c1a441308af7c2e10a8023c70
ecknomichiel <michiel@frostvikenit.eu>
2017-07-19 13:34:25 +0200

    Punctuation games works.

--- Gamification_MVC/Scripts/Angular/Games/Punctuation/PunctuationGame.js
+++ Gamification_MVC/Scripts/Angular/Games/Punctuation/PunctuationGame.js
@@ -1,26 +1,47 @@
 ﻿var app = angular.module("Gamification");
 
 app.controller("PunctuationGameController", ["$scope", "$http", function ($scope, $http) {
-    $scope.sentences = ["Unicorn tests are the best!", 
+    sentences = ["Unicorn tests are the best!", 
         "I would like to have another sentence. One that has other puctuation marks in it.",
         "A subordinated clause is a clause that has no meaning if you take away its best friend, the main clause.",
     "Sometimes you would like to give an example, such as: 'this is an exampe'."];
+    $scope.questionnaire = $scope.sentences;
     $scope.getRandomSentence = function (){
         idx = Math.floor(Math.random() * $scope.sentences.length);
 
-        result = $scope.sentences[idx];
+        result = sentences[idx];
         return result;
     };
 
     $scope.getNextQuestion = function () {
-        idx = Math.floor(Math.random() * $scope.sentences.length);
-        $scope.currentSentence = $scope.sentences[idx];
-        $scope.sentences.delete
-        $scope.currentQuestion = $scope.currentSentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "*");
+        if ($scope.questionnaire.length == 0) {
+            $scope.currentSentence = "";
+            $scope.message = "All questions answered. You got " + $scope.score + " out of " + sentences.length + " right.";
+        }
+        idx = Math.floor(Math.random() * $scope.questionnaire.length);
+        $scope.currentSentence = $scope.questionnaire[idx];
+        $scope.questionnaire.splice(idx, 1);
+        $scope.answer = $scope.currentSentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "*");
+        $scope.message = "";
     };
 
+    $scope.start = function () {
+        $scope.questionnaire = sentences.slice();
+        $scope.score = 0;
+        $scope.getNextQuestion();
+    };
 
+    $scope.checkAnswer = function () {
+        if ($scope.currentSentence == $scope.answer)
+        {
+            $scope.message = "Correct";
+            $scope.score++;
+        }
+        else
+        {
+            $scope.message = "Incorrect. The right answer was: " + $scope.currentSentence;
+        }
+    };
 
-    $scope.currentSentence = $scope.getRandomSentence();
-    $scope.score = 0;
+    $scope.start();
 }]);
